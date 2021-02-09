0252577308cf0df1a52ae2dc75bcb20584d380e6
harukoya <ka.uni.stu@gmail.com>
2021-02-10 08:10:29 +0900

    [Add] 2021/02/10

--- ALDS_1_5_A/ALDS_1_5_A.cpp
+++ ALDS_1_5_A/ALDS_1_5_A.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int n;
+int A[50];
+
+int solve(int i, int m) {
+  //  ベースケース
+  if (m == 0) {
+    return 1;
+  }
+  if (i >= n) {
+    return 0;
+  }
+  int result = solve(i + 1, m) || solve(i + 1, m - A[i]);
+  return result;
+}
+
 int main() {
-  int n, q;
+  int q;
   cin >> n;
-  int A[n];
   for (int i = 0; i < n; i++) {
     cin >> A[i];
   }
@@ -14,5 +28,13 @@ int main() {
     cin >> m[i];
   }
 
+  for (int i = 0; i < q; i++) {
+    if (solve(0, m[i])) {
+      cout << "yes" << endl;
+    } else {
+      cout << "no" << endl;
+    }
+  }
 
+  return 0;
 }
